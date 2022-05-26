0da2b486d0025210486726f2f597979e8bc50215
Luis Manzo <manzosk3@gmail.com>
2022-05-26 17:03:16 -0500

    pair programming with Angel Felix

--- js/characterMatch.js
+++ js/characterMatch.js
@@ -1,3 +1,6 @@
-exports.isCharacterMatch = function(string1, string2) {
-
+function isCharacterMatch(string1, string2) {
+  return (string1.toLowerCase().split('').sort().join('') === string2.toLowerCase().split('').sort().join(''));
 };
+
+console.log(isCharacterMatch('charm', 'mArch'));
+console.log(isCharacterMatch('abcde2', 'c2abed'));
