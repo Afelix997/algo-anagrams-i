d93385a4388772c8f1ea7e6f59734c3e56e76e2a
afelix997 <afelix2013@gmail.com>
2022-05-29 18:27:58 -0700

    answer

--- python/character_match.py
+++ python/character_match.py
@@ -1,5 +1,9 @@
-# Don't forget to run your tests!
-
 def is_character_match(string1, string2):
-	pass
-	# Your code here
+	new_string1= list(string1.lower())
+	new_string1.sort()
+	new_string2= list(string2.lower())
+	new_string2.sort()
+	new_string1="".join(new_string1)
+	new_string2="".join(new_string2)
+	return new_string1.replace(" ", "")== new_string2.replace(" ", "")
+
